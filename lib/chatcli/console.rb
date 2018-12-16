37aa09105b79eec467f03c87d8e5a70cbfb25615
Nic Hampton <nicolasjhampton@gmail.com>
2018-12-15 18:22:08 -0800

    starting join

--- lib/chatcli/console.rb
+++ lib/chatcli/console.rb
@@ -1,17 +1,26 @@
+require 'json'
+
 module Chatcli
   class Console
     def initialize; end
 
-    attr_reader :client
+    attr_accessor :client, :status
 
     def login
       puts 'Enter a username'
       username = gets.chomp
       @client = Client.new username
-      login_info = @client.login
+      @client.login
       user_info = @client.info
-      puts login_info
-      puts user_info
+      @status = JSON.parse(user_info)["results"]
+      group_listing
+    end
+
+    def group_listing
+      puts @status
+      @status["users"].each_with_index do |user, index|
+        puts "#{index + 1}. #{user}"
+      end
     end
   end
 end
